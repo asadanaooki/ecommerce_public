c6ca2d1d149a429e1b3f370c473d6d763ac12f88
naokiasada <siotan0922@yahoo.co.jp>
2025-05-09 14:32:20 +0900

    初期コミット

---
+++ src/test/java/com/example/EcommercePublicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommercePublicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
